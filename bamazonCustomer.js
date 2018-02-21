3f76ba09bf73a7d90b8666aa9fe985c3d6b8b65e
Robert Castellano <castellanorob@gmail.com>
2018-02-20 21:29:09 -0600

    Updated bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,33 @@
 // Requiring inquirer and mysql
-
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+// Creating a connection to mysql
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "bamazon"
+});
+  
+connection.connect(function(err) {
+  if (err) throw err;
+  showProducts();
+});
+
+// Displaying the products table
+function showProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+}
 
